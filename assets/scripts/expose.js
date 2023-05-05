d351861faba8ce70a2c8abc844b5c2271ac365c2
dflores1229 <d.flores1229@gmail.com>
2023-05-05 12:44:11 -0700

    changes horn image and volume levels

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,60 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelector = document.getElementById('horn-select');
+  const hornImg = document.querySelectorAll("img")[0];
+  const hornAudio = document.querySelector("audio");
+
+  let volumeSlider = document.getElementById("volume");
+  const volImg = document.querySelectorAll("img")[1];
+  
+  hornSelector.addEventListener( "change", changeHorn);
+  volumeSlider.addEventListener("input", changeVol);
+  
+
+  function changeHorn(){
+    if( hornSelector.value === "air-horn"){
+      hornImg.src = "assets/images/air-horn.svg";
+      hornAudio.src = "assets/audio/air-horn.mp3"
+      hornImg.alt = "air horn image";
+    }
+    else if( hornSelector.value === "car-horn"){
+      hornImg.src = "assets/images/car-horn.svg";
+      hornAudio.src = "assets/audio/car-horn.mp3" 
+      img.alt = "car horn image";
+    }
+    else if( hornSelector.value === "party-horn"){
+      hornImg.src = "assets/images/party-horn.svg";
+      hornAudio.src = "assets/audio/party-horn.mp3"
+      img.alt = "party horn image";
+    }
+    else{
+      hornImg.src = "assets/images/no-image.svg";
+      img.alt = "no image displayed";
+    }
+  }
+
+  function changeVol(){
+    let val = volumeSlider.value;
+    hornAudio.volume = val/100;
+
+    if( 1 <= val && val < 33){
+      volImg.src = "assets/icons/volume-level-1.svg";
+      img.alt = "volume level 1";
+    }
+    else if( val < 67){
+      volImg.src = "assets/icons/volume-level-2.svg";
+      img.alt = "volume level 2";
+    }
+    else if( 66 < val){
+      volImg.src = "assets/icons/volume-level-3.svg";
+      img.alt = "volume level 3";
+    }
+    else{
+      volImg.src = "assets/icons/volume-level-0.svg";
+      img.alt = "no volume";
+    }
+  }
+
 }
+
